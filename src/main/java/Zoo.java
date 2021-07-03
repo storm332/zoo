901f4e1b79ae1e0ddea959a989e76309aa743d6d
dvotino <dvotino@tai.it>
2021-07-03 15:08:33 +0200

    inizializzato programma

---
+++ src/main/java/Zoo.java
@@ -0,0 +1,26 @@
+public class Zoo {
+
+    public static void main(String[] args) {
+
+        // installare le strutture
+
+        // 1 - gabbie
+
+        // 2 - biglietteria
+
+        // 3 - magazzino
+
+        // Riempi il magazzino
+
+        // Riempi le gabbie
+
+        // Assumi personale
+
+        // Avvia lavoratori
+
+        // apri biglietteria
+
+
+    }
+
+}
